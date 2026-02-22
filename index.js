4714a315713eee76d96f59e5a416d942d8438ad5
Surya Prakash Pareek <139848993+Surya-prakash-pareek@users.noreply.github.com>
2026-02-22 21:52:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 export default function handler(req, res) {
-  console.log("Email requested");
-
-  res.status(200).send("22f2001613@iitm.ds.study.iitm.ac.in");
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send("22f2001613@ds.study.iitm.ac.in");
 }
